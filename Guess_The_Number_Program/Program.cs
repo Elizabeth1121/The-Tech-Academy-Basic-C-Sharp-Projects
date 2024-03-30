c94a6ac2454b02597681f01cd5570401cca2754d
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-03-30 14:46:50 -0400

    Add files via upload
    Added guessing game for Boolean and while loops.

---
+++ Guess_The_Number_Program/Program.cs
@@ -0,0 +1,36 @@
+﻿//creating random number
+Random random = new Random();
+int randomNumber = random.Next(1, 100);
+
+// Start of the game
+Console.WriteLine("Welcome to the Number Guessing Game! \n \nI have selected a random number between 1 and 100." +
+    "\nCan you guess the number? \nPlease enter your guess: ");
+
+// take input from user
+int userGuess = Convert.ToInt32(Console.ReadLine());
+
+// check if the user guess is correct
+while(userGuess != randomNumber)
+{
+    // if correct
+    if (userGuess == randomNumber)
+    {
+        break;
+    }
+
+    // if too low
+    if(userGuess < randomNumber)
+    {
+        Console.WriteLine("Your guess is too low. Please try again: ");
+        userGuess = Convert.ToInt32(Console.ReadLine());
+    }
+    // if too high
+    else
+    {
+        Console.WriteLine("Your guess is too high. Please try again: ");
+        userGuess = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+// you win!
+Console.WriteLine("Congratulations! You have guessed the correct number. The number was: " + randomNumber);
