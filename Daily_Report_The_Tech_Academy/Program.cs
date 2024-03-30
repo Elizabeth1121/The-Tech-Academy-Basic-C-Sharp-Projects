47831a26066b8afbbeb152024a52bd7f08096d7e
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-03-29 21:36:05 -0400

    Add files via upload
    Added the daily student report to the repo..

---
+++ Daily_Report_The_Tech_Academy/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("The Tech Acedemy");
+
+// Student Daily Report
+Console.WriteLine("Student Daily Report");
+
+// student name
+Console.WriteLine("What is your name?");
+string studentName = Console.ReadLine();
+
+// student course
+Console.WriteLine("What course are you on?");
+string studentCourse = Console.ReadLine();
+
+// student page number
+Console.WriteLine("What page number?");
+string studentPageNumber = Console.ReadLine();
+
+// student help
+Console.WriteLine("Do you need help with anything? Please answer 'true' or 'false'.");
+string studentHelp = Console.ReadLine();
+
+// student feedback
+Console.WriteLine("Is there any feedback you would like to share? If so, give specifics");
+string studentFeedback = Console.ReadLine();
+
+// student hours
+Console.WriteLine("How many hours did you study today?");
+string studentHours = Console.ReadLine();
+
+// final response to student
+Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
