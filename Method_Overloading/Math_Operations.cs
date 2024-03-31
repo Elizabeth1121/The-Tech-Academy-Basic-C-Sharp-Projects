3340ca791556048f4efd3330d3fed826118f10e6
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-03-31 15:28:06 -0400

    Add files via upload
    added method overloading program.

---
+++ Method_Overloading/Math_Operations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading
+{
+    public class Math_Operations
+    {
+        // Method to add two integers
+        public int Add(int num1)
+        {
+            return num1 + 5;
+        }
+
+        // Method with the sname name but takes in a decimal
+        public decimal Add(decimal num1)
+        {
+            return num1 + 5;
+        }
+
+        // Method with the same name but takes in a string
+        public string Add(string num1)
+        {
+            return num1 + "5";
+        }
+    }
+}
---
+++ Method_Overloading/Program.cs
@@ -0,0 +1,14 @@
+﻿using Method_Overloading;
+
+
+// Create an instance of the Math_Operations class
+Math_Operations math = new Math_Operations();
+
+// Call the Add method and pass in an integer
+Console.WriteLine(math.Add(5));
+
+// Call the Add method and pass in a decimal
+Console.WriteLine(math.Add(5.5m));
+
+// Call the Add method and pass in a string
+Console.WriteLine(math.Add("5"));
