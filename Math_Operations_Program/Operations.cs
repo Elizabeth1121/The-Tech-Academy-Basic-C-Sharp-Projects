f24d7e719f62ed1b430e93eedeeb9754b50c276a
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-03-31 14:59:06 -0400

    Add files via upload
    Added the math operations program to my repo.

---
+++ Math_Operations_Program/Operations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Operations_Program
+{
+    public  class Operations
+    {
+        //Addition
+        public int Add(int a)
+        {
+            return a + a;
+        }
+
+        // subtraction
+        public int Subtract (int a)
+        {
+            return a - 1;
+        }
+
+        // Multiplication
+        public int Multiply (int a)
+        {
+            return a * a;
+        }
+    }
+}
---
+++ Math_Operations_Program/Program.cs
@@ -0,0 +1,19 @@
+﻿using Math_Operations_Program;
+
+
+// creating an instance of the Operations class
+Operations operations = new Operations();
+
+// Prompting the user to enter a number
+Console.WriteLine("Hello! please pick a number you would like to do math operations on.");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// calling the methods from the Operations class
+int numberAddition = operations.Add(number);
+int numberSubtraction = operations.Subtract(number);
+int numberMultiplication = operations.Multiply(number);
+
+// Displaying the results
+Console.WriteLine("For addition the anwser is: " + numberAddition);
+Console.WriteLine("For subtraction the anwser is: " + numberSubtraction);
+Console.WriteLine("For multiplication the anwser is: " + numberMultiplication);
