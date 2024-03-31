bc004b9665d32465b46828f7080ecce0001bdc7c
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-03-31 15:39:02 -0400

    Add files via upload
    Added the multiple parameters program to my repo.

---
+++ Multiple_Parameters/Math_Operations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiple_Parameters
+{
+    public class Math_Operations
+    {
+        // Method to add two numbers, one number is optional
+        public int Add(int num1, int num2 = 3)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ Multiple_Parameters/Program.cs
@@ -0,0 +1,17 @@
+﻿using Multiple_Parameters;
+
+Math_Operations math = new Math_Operations();
+
+// user input
+Console.WriteLine("Enter a number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+// user input
+Console.WriteLine("Enter a second number (this is optional, press enter if you want to leave this blank): ");
+string num2 = Console.ReadLine();
+
+// if the user leaves the second number blank, the default value of 3 will be used
+int result = math.Add(num1, num2 == "" ? 3 : Convert.ToInt32(num2));
+
+// output
+Console.WriteLine("The result is: " + result);
