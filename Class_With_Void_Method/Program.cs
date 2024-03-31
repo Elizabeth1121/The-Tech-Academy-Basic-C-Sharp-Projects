001add1a33329ab4dad66dc9b12ae9ca988ec4dc
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-03-31 15:43:49 -0400

    Add files via upload
    Added my class with void methods program to my repo.

---
+++ Class_With_Void_Method/Program.cs
@@ -0,0 +1,8 @@
+﻿using Class_With_Void_Method;
+
+
+// creating an instance of the class
+Void_Method obj = new Void_Method();
+
+// calling the method
+obj.Add(10, 20);
---
+++ Class_With_Void_Method/Void_Method.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_With_Void_Method
+{
+    public class Void_Method
+    {
+        // addition method of type void
+        public void Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine("The sum of {0} and {1} is {2}", num1, num2, result);
+        }
+    }
+}
