2d83c1d67a3696ce35029aaab6f7cb5609ac0cd8
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-04-02 01:12:40 -0400

    Add files via upload
    Added Overloading Operators Program to my repo.

---
+++ Overloading_Operators/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Operators
+{
+    // create an employee class
+    public class Employee
+    {
+        // create properties for the employee class
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public int ID { get; set; }
+
+        // create an overload for the == operator
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            return employee1.ID == employee2.ID;
+        }
+
+        // create an overload for the != operator
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            return employee1.ID != employee2.ID;
+        }
+
+    }
+}
---
+++ Overloading_Operators/Program.cs
@@ -0,0 +1,16 @@
+﻿using Overloading_Operators;
+
+// create two employee objects
+Employee employee = new Employee();
+employee.ID = 1;
+employee.firstName = "Joe";
+employee.lastName = "Smith";
+
+Employee employee2 = new Employee();
+employee2.ID = 2;
+employee2.firstName = "Joe";
+employee2.lastName = "Smith";
+
+// compare the two employee objects
+Console.WriteLine(employee == employee2);
+Console.WriteLine(employee != employee2);
