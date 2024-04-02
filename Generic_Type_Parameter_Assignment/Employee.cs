0ac64f962693f1eed5f59db049f43a30094348c9
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-04-02 17:10:21 -0400

    Add files via upload
    Added my Generic Type Parameter Assignment to my repo.

---
+++ Generic_Type_Parameter_Assignment/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Type_Parameter_Assignment
+{
+    // creating a generic class called Employee
+    public class Employee<T>
+    {
+        // creating a property list of type T called things
+       public List<T> things = new List<T>();
+    }
+}
---
+++ Generic_Type_Parameter_Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using Generic_Type_Parameter_Assignment;
+
+// creating an instance of the Employee class with a type parameter of string
+Employee<string> employee = new Employee<string>();
+// adding items to the list of type string
+employee.things.Add("Hello");
+employee.things.Add("World");
+employee.things.Add("!");
+
+// creating an instance of the Employee class with a type parameter of int
+Employee<int> employee2 = new Employee<int>();
+// adding items to the list of type int
+employee2.things.Add(1);
+employee2.things.Add(2);
+employee2.things.Add(3);
+employee2.things.Add(4);
+
+// printing the items in the list of type string
+foreach (string thing in employee.things)
+{
+    Console.WriteLine(thing);
+}
+
+// printing the items in the list of type int
+foreach (int thing in employee2.things)
+{
+    Console.WriteLine(thing);
+}
