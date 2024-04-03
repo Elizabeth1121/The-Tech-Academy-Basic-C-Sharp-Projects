77f786525ec6a0481142afc115abda0ab4083291
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-04-02 21:24:03 -0400

    Add files via upload
    Added my struct program to the repo.

---
+++ Struct_Program/Program.cs
@@ -0,0 +1,15 @@
+﻿// create a struct with a property that has a getter and a setter
+Number number = new Number();
+
+// set the value of the property
+number.Amount = 100.0m;
+
+// print the value of the property
+Console.WriteLine(number.Amount);
+
+// define a struct
+struct Number
+{
+    // define a property with a getter and a setter
+    public decimal Amount { get; set; }
+}
