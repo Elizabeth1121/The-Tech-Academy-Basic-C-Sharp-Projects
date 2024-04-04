abe7871c6d000de6e1975eb180c0a8e07fa79e42
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-04-04 15:29:54 -0400

    Add files via upload
    Added my try catch assignment to the repo.

---
+++ TryCatch_Assignment/NegativeAgeException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch_Assignment
+{
+    // NegativeAgeException class
+    public class NegativeAgeException : Exception
+    {
+        // NegativeAgeException constructor
+        public NegativeAgeException() : base() { }
+
+        // NegativeAgeException constructor with message
+        public NegativeAgeException(string message) : base(message) { }
+    }
+}
---
+++ TryCatch_Assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using TryCatch_Assignment;
+
+// asking user for their age
+Console.WriteLine("Please enter your age: ");
+
+// try catch block to catch any exceptions
+try
+{
+    // user input
+	int userInput = Convert.ToInt32(Console.ReadLine());
+
+    // if user input is less than or equal to 0, throw exception
+    if (userInput <= 0)
+    {
+        throw new NegativeAgeException();
+    }
+    // if user input is greater than 0, display user input
+    else
+    {
+        Console.WriteLine("Your age is... {0}", userInput);
+    }
+}
+// catch NegativeAgeException
+catch (NegativeAgeException)
+{
+    // display error message
+    Console.WriteLine("Age is not valid, age cannot be zero or a negative number");
+}
+// catch any other exceptions
+catch (Exception)
+{
+    // display error message
+    Console.WriteLine("User input was incorrect, please review your input and try again.");
+}
