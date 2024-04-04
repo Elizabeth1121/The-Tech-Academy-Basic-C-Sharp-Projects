549deeb9b58e847ee3d3645839de650e6bc43ffb
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-04-03 23:44:32 -0400

    Add files via upload
    Added writing to a txt file assignment to my repo.

---
+++ Writing_Txt_To_A_File_Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+// Ask the user for a number
+Console.WriteLine("Hello user! Please type a number: ");
+// Read the number from the user
+int usernumber = Convert.ToInt32(Console.ReadLine());
+
+// Write the number to a file -- **change the path to your file**
+using (StreamWriter file = new StreamWriter(@"C: \path\to\your\file.txt"))
+{
+    // Write the number to the file
+    file.WriteLine(usernumber);
+}
