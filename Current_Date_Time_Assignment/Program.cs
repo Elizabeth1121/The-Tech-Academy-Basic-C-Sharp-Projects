83420bbae1a3e1a4ed58b29a72374eea5e67784d
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-04-04 00:07:06 -0400

    Add files via upload
    Added my current date time assignment to the repo.

---
+++ Current_Date_Time_Assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// getting the current time
+DateTime currentTime = DateTime.Now;
+
+// printing the current time
+Console.WriteLine(currentTime);
+
+// askin the user for a number
+Console.WriteLine("Please give me a number: ");
+
+// reading the number from the user
+int number = int.Parse(Console.ReadLine());
+
+// printing the time after the number of hours
+Console.WriteLine(currentTime.AddHours(number) + " hours from now");
