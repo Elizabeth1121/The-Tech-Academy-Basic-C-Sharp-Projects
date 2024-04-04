9beca4d8744c86bee46b84e873eeee24decc367b
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-04-04 14:45:09 -0400

    Add files via upload
    Added and updated the constructor assignment to my repo.

---
+++ Constructor_Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using Constructor_Assignment;
+
+// creating a const string variable
+const string input = "Hello , World!";
+
+// creating a new instance of the Person class
+var person = new Person(Console.ReadLine(), Console.ReadLine());
+
+// printing the values of the fName and lName fields of the person object
+Console.WriteLine(person.fName + " " + person.lName);
+
+// creating a new instance of the Person class with the default constructor
+var person1 = new Person();
+
+// printing the values of the fName and lName fields of the person1 object
+Console.WriteLine(person1.fName + " " + person1.lName);
+
+// printing the value of the input variable
+Console.WriteLine(input);
