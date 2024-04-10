504edc3b0a44e01b269aa77edad996f45ae63c30
elizabeth lynch <itsybitsy1121@outlook.com>
2024-04-10 14:21:38 -0400

    Added the WebAppChallenge1 to repo

---
+++ WebAppChallenge1/WebAppChallenge1/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppChallenge1.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+
+    // write a method to return SeverTime
+    public string GetServerTime()
+    {
+        // create a varibale to store the current time
+        var ServerTime = DateTime.Now;
+        
+        // return the current time
+        return ServerTime.ToString();
+    }
+}
